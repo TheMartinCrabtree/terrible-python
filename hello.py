62c99db86e5682662ae91164bc8be5fdfd9c6bdf
Martin Crabtree <mcrabtr33@gmail.com>
2020-02-18 20:03:56 -0500

    string interpolation

--- hello.py
+++ hello.py
@@ -21,16 +21,23 @@
 
 
 # multi_list = [["a","b","c"], [1,2,3]["blue", "green", "yellow"]]
-multi_list = [["a","b","c"], ["1","2","3"],["blue", "green", "yellow"]]
+# multi_list = [["a","b","c"], ["1","2","3"],["blue", "green", "yellow"]]
 
-for firstElem in multi_list:
-    for secondElem in firstElem:
-        print(secondElem)
+# for firstElem in multi_list:
+#     for secondElem in firstElem:
+#         print(secondElem)
 
-# for innerElem in multi_list:
-#     print(len(innerElem))
+# # for innerElem in multi_list:
+# #     print(len(innerElem))
 
-print(multi_list[-1])
+# print(multi_list[-1])
 
+
+prime_numbers = [2, 3, 5, 7, 11]
+outputStr = ""
+
+for each_num in prime_numbers:
+    print(f"The square of {each_num} is {each_num**2}")
+    
 
 
